1a263c1f8f04591fd6a0687b8af83c8154687270
Schönberger  Florian <fschoenberge@ethz.ch>
2025-11-04 11:06:17 +0100

    lösung task 1

--- app.py
+++ app.py
@@ -65,9 +65,14 @@ def using_post():
 
 
 # TODO: TASK 1
-# @app.route("/round_float", methods=["GET"])
-# def round_float():
-#     ...
+@app.route("/round_float", methods=["GET"])
+def round_float():
+    try:
+        round_float = float(request.args.get("value", 1))
+    except ValueError:
+        return jsonify("Invalid input for num1")
+    rounded= round(round_float,2)
+    return jsonify({"output": rounded})
 
 # TODO: TASK 2 - one method for increase and one method for decrease value
 
